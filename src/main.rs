dbeecd6e57aee9bee0119109f627f73600d6407a
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-08-19 22:01:13 +0300

    Modify hardcoded port.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use versa_relay::TcpRelay;
 
 fn main() {
-    let tcp_relay = TcpRelay::new("192.168.1.35".to_string(), 8095);
+    let tcp_relay = TcpRelay::new("192.168.1.35".to_string(), 1080);
     tcp_relay.start().unwrap();
 }
