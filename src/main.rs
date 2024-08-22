c02f73bb4975460604be0c1efa18ac452af55a98
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-08-22 03:32:06 +0300

    Fix formatting.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use versa_relay::{TcpRelay, ThreadPool};
 
 fn main() {
-    let thread_pool = ThreadPool::builder().max_threads(256).idle_threads(8).build();
+    let thread_pool = ThreadPool::builder()
+        .max_threads(256)
+        .idle_threads(8)
+        .build();
     let tcp_relay = TcpRelay::new(thread_pool, "192.168.1.35".to_string(), 1080);
     tcp_relay.start();
 }
