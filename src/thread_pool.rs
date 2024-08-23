35f8af4064898707a206258a67cc5c01fd5d8d79
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-08-24 02:00:03 +0300

    Clean up exceeding threads by killing them with sending through channel tasks that will panic.

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -195,5 +195,17 @@ impl ThreadPool {
                 self.spawn_worker();
             }
         }
+
+        // Kill exceeding workers if necessary.
+        if idle_workers > 2 * self.max_idle_workers {
+            let workers_to_kill =
+                (total_workers - busy_workers).saturating_sub(self.max_idle_workers);
+            println!("[ThreadPool] Sending {workers_to_kill} suicide requests.");
+            for _ in 0..workers_to_kill {
+                self.sender
+                    .send(Box::new(|| panic!("exceeding threads cleanup"))) // TODO: Task(Task)/Abort enum.
+                    .unwrap();
+            }
+        }
     }
 }
